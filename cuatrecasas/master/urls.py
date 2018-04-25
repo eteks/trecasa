60eb97828afca141b937350535b3f9ea4aae86fb
Banumathy M <banu@etekchnoservices.com>
2018-04-25 11:19:12 +0530

    files added

---
+++ cuatrecasas/master/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url,include
+from master import views
+from django.views.decorators.csrf import csrf_exempt
+
+urlpatterns = [    
+    url(r'^$', views.Index_pageview.as_view(), name='index'),
+   
+    ]
--- cuatrecasas/master/views.py
+++ cuatrecasas/master/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView # Import TemplateView
+
 
 # Create your views here.
+class Index_pageview(TemplateView):
+    template_name = "index.html"
